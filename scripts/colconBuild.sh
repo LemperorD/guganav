63b8061eaaf08b9aa7ccb94840eb004ec463ba02
LemperorD <ld2382619813@163.com>
2026-07-23 10:37:31 +0800

    feat: 更新构建脚本并删除不再使用的可视化脚本

--- scripts/colconBuild.sh
+++ scripts/colconBuild.sh
@@ -2,5 +2,5 @@
 # guganav构建脚本
 
 cd ~/guganav
-colcon build --symlink-install \
-  --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+sudo ln -s ~/guganav/build/compile_commands.json ~/guganav/compile_commands.json
