ffe2d61897c3af4f0a3f81719ddfa2bb1d0688cd
LemperorD <ld2382619813@163.com>
2026-07-24 23:28:47 +0800

    fix(common): 修复头文件的包含保护宏

--- src/guga_common/include/guga_common/common_libs.hpp
+++ src/guga_common/include/guga_common/common_libs.hpp
@@ -1,3 +1,6 @@
+#ifndef GUGA_COMMON_COMMON_LIBS_HPP
+#define GUGA_COMMON_COMMON_LIBS_HPP
+
 #include <string>
 
 /// ANSI终端颜色控制码：红色，用于错误输出
@@ -10,3 +13,5 @@ const std::string YELLOW = "\033[1;33m";
 const std::string BLUE = "\033[1;34m";
 /// ANSI终端颜色重置码
 const std::string RESET = "\033[0m";
+
+#endif // GUGA_COMMON_COMMON_LIBS_HPP
