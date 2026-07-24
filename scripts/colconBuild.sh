4fbcc8639ec3596df9e4475eacfcd34352c0d9cf
LemperorD <ld2382619813@163.com>
2026-07-24 23:28:19 +0800

    chore(scripts): :wrench: 修改构建脚本
    不需要sudo权限即可运行
    
    未来会添加删除原有构建重新编译的功能

--- scripts/colconBuild.sh
+++ scripts/colconBuild.sh
@@ -3,4 +3,7 @@
 
 cd ~/guganav
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-sudo ln -s ~/guganav/build/compile_commands.json ~/guganav/compile_commands.json
+if [ ! -L /home/ld/guganav/compile_commands.json ]; then
+  ln -s  ~/guganav/build/compile_commands.json ~/guganav/compile_commands.json
+  echo "已创建compile_commands.json的软链接, 请设置vscode的c_cpp_properties.json以启用代码补全和跳转功能。"
+fi
