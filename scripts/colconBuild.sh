75b79fbad5638e496d49516000787faa2894d1c0
LemperorD <ld2382619813@163.com>
2026-07-31 00:27:28 +0800

    feat(scripts): :sparkles: 给构建脚本添加新功能
    可以输入参数删除已有构建目录

--- scripts/colconBuild.sh
+++ scripts/colconBuild.sh
@@ -1,6 +1,60 @@
 #!/bin/bash
 # guganav构建脚本
 
+show_help() {
+cat << EOF
+Usage:
+    <workspace>/scripts/colconBuild.sh [OPTIONS]
+
+Options:
+    -h / --help          显示帮助
+    -c / --clean         清理build目录
+    -r / --release       Release编译
+    -d / --debug         Debug编译
+EOF
+}
+
+# 默认参数
+CLEAN=false
+BUILD_TYPE=Release
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        -c | --clean)
+            CLEAN=true
+            shift
+            ;;
+        -r | --release)
+            BUILD_TYPE=Release
+            shift
+            ;;
+        -d | --debug)
+            BUILD_TYPE=Debug
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Use --help for usage."
+            exit 1
+            ;;
+    esac
+done
+
+echo "Build type : $BUILD_TYPE"
+echo "Clean      : $CLEAN"
+
+if $CLEAN; then
+  echo "[warn]maybe need ur password"
+  sudo rm -rf build/ log/ install/
+  echo "Cleaning..."
+fi
+
+echo "Building..."
+
 cd ~/guganav
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 if [ ! -L /home/ld/guganav/compile_commands.json ]; then
