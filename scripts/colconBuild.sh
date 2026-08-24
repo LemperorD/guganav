696dbc81dfbe56cc7e4e2b33d5f04d63a8e3e72e
freedegit <3340048769@qq..com>
2026-08-24 20:19:40 +0800

    colcon修复

--- scripts/colconBuild.sh
+++ scripts/colconBuild.sh
@@ -148,7 +148,7 @@ colcon build ${BUILD_ARGS[*]} \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\
 "
-if [$? -ne 1 ]; then
+if [ $? -ne 1 ]; then
   echo "Selected packages build failed, please check the error messages."
   exit 1
 fi
@@ -160,7 +160,7 @@ export MAKEFLAGS='-j1 -l1'
 colcon build \
     --packages-ignore $PACKAGES_SELECT \
     --symlink-install \
-    --executor sequential \
+    --parallel-workers 4 \
     --cmake-args \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
